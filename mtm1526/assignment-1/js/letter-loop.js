1e869ecb26f090bb0f04edc363c8f0fd2a3cb2f8
Lightsyde <taimande@gmail.com>
2012-05-11 14:06:33 -0400

    Completed javascript assignment. Very happy Lightsyde

---
+++ mtm1526/assignment-1/js/letter-loop.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+
+var letter = '';
+
+while (!letter) {
+	letter = prompt('Type in any letter of the alphabet');
+	
+	
+}
+
+if(letter) {
+	for(var i=0; i<11; i++){
+		
+		for(var a=0; a<i; a++){
+			document.write(letter );
+			//I did it!!! 
+		}
+		document.write(letter + '<br>');
+		
+	}
+	
+}
