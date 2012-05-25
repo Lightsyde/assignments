3de53b35e44a32b841ace5ca75ecb2f686229683
Lightsyde <taimande@gmail.com>
2012-05-25 12:31:10 -0400

    finished working with the apply button created circle

--- mtm1526/assignment-3/js/simple-app.js
+++ mtm1526/assignment-3/js/simple-app.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
 	
+	$('form').on('submit', function (ev) {
+		ev.preventDefault();
+		
+	})
 	
+	$('#apply').on('click', function (ev) {
+		
+		var itemIs = $('#property').val();
+		console.log(itemIs);
+		var colorValue = $('#color-box').val();
+		
+		var colorChange = $('.circ').css(itemIs, colorValue);
+		
+		if(itemIs == 'background-color' && $('#color-box').val() != null) {
+			
+			colorChange;
+			
+		}
+		else if(itemIs == 'border-color' && $('#color-box').val() != null) {
+			
+			colorChange;
+		}
+		else if(itemIs == 'color' && $('#color-box').val() != null) {
+			
+			//$('.circ').css('color', colorValue);
+			colorChange;
+		}
+	});
 	
 });// JavaScript Document
