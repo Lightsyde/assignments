9c832d2c2eac020186608bfb97272aa93e029ed2
Lightsyde <taimande@gmail.com>
2012-06-04 14:11:38 -0400

    created database php file and index.php file

---
+++ mtm1531/assignment-5/index.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'includes/database.php';
+
+?><!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Movie Browser</title>
+	</head>
+
+	<body>
+		<h2></h2>
+		<dl>
+			<dt>Movie Title</dt>
+			<dd></dd>
+			<dt>Release Date</dt>
+			<dd></dd>
+			<dt>Director</dt>
+			<dd></dd>
+			
+		</dl>
+	</body>
+</html>
