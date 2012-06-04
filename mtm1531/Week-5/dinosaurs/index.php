8d0659ad14be568dd5a09da3bf5110734a552767
Lightsyde <taimande@gmail.com>
2012-06-04 12:54:28 -0400

    created index.php and the database connection php file, 'db'

---
+++ mtm1531/Week-5/dinosaurs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+	<head>
+		<meta charset="utf-8" />
+		<title>Dinosaurs</title>
+	</head>
+
+	<body>
+		<h2>Velociraptor</h2>
+		<dl>
+			<dt>Loves Meat</dt>
+			<dd>1</dt>
+			<dt>In Jurassic Park</dt>
+			<dd>1</dt>
+		
+		</dl>
+	
+	php doc
+	</body>
+</html>
