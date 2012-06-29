f514f413e50c42ad84fbc2982d855d49b09900ef
Lightsyde <taimande@gmail.com>
2012-06-29 16:06:57 -0400

    can someone say 'countries'?

--- mtm1526/assignment-5/countries.php
+++ mtm1526/assignment-5/countries.php
@@ -22,25 +22,25 @@
 </div>
 
 <div id="addus">
-	<label for="province">Province</label>
+	<label for="state">State</label>
 	<select>
-		<option value="al">Alberta</option>
-		<option value="bc">British Columbia</option>
-		<option value="on">Ontario</option>
-		<option value="man">Manitoba</option>
-		<option value="nb">New Brunswick</option>
-		<option value="nf">Newfoundland</option>
-		<option value="ns">Nova Scotia</option>
-		<option value="nwt">North-West Territory</option>
-		<option value="nun">Nunavut</option>
-		<option value="pei">Prince Edward Island</option>
-		<option value="qu">Quebec</option>
-		<option value="sask">Sasketchewan</option>
-		<option value="yukon">Yukon</option>
+		<option value="ala">Alabarma</option>
+		<option value="cal">California</option>
+		<option value="del">Delaware</option>
+		<option value="flo">Florida</option>
+		<option value="geo">Georgia</option>
+		<option value="haw">Hawai</option>
+		<option value="ill">Illinois</option>
+		<option value="kan">Kansas</option>
+		<option value="lou">Louisiana</option>
+		<option value="mai">Maine</option>
+		<option value="qu">Nebraska</option>
+		<option value="ohi">Ohio</option>
+		<option value="pen">Pennslyvania</option>
 	</select>
 	<div>
-		<label for="postalcode">Postal code</label>
-		<input id="postalcode" name="postalcode" required>
+		<label for="zipcode">Zip code</label>
+		<input id="zipcode" name="zipcode" required>
 	</div>
 </div>
 
